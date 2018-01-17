713f44b66553622de6ee9295f8233f0f605361be
Marcin Kołodziej <dev@izoslav.pl>
2018-01-17 18:14:40 +0100

    add imgui example

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,41 @@
+#include <imgui/imgui.h>
+#include <imgui/imgui-SFML.h>
+
 #include <SFML/Graphics.hpp>
 
 int main()
 {
-  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+  sf::RenderWindow window(sf::VideoMode(800, 600), "Gridlocked");
+  window.setFramerateLimit(60u);
+
+  ImGui::SFML::Init(window);
+
   sf::CircleShape shape(100.f);
   shape.setFillColor(sf::Color::Green);
 
+  sf::Clock deltaClock;
   while (window.isOpen())
   {
     sf::Event event;
     while (window.pollEvent(event))
     {
+      ImGui::SFML::ProcessEvent(event);
+
       if (event.type == sf::Event::Closed)
         window.close();
     }
 
+    ImGui::SFML::Update(window, deltaClock.restart());
+
+    ImGui::Begin("Hello, world!");
+    ImGui::Button("Look at this pretty button");
+    ImGui::End();
+
     window.clear();
     window.draw(shape);
+    ImGui::SFML::Render(window);
     window.display();
   }
-
-  return 0;
 
   return 0;
 }
